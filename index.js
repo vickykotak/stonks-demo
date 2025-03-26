04280309f8e95128bd4288bd21a718f7409fb8ff
Vicky <vickykotak09@gmail.com>
2025-03-26 10:41:32 +0530

    pre load

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-import image1 from "./assets/image1.jpg";
-import image2 from "./assets/image2.jpg";
-import image3 from "./assets/image3.jpg";
-import image4 from "./assets/image4.jpg";
-import image5 from "./assets/image5.jpg";
-import image6 from "./assets/image6.jpg";
-import image7 from "./assets/image7.jpg";
-import image8 from "./assets/image8.jpg";
-import image9 from "./assets/image9.jpg";
-import image10 from "./assets/image10.jpg";
-import image11 from "./assets/image11.jpg";
-
 let pointer = 0;
 let images = [
-  "image1",
-  "image2",
-  "image3",
-  "image4",
-  "image5",
-  "image6",
-  "image7",
-  "image8",
-  "image9",
-  "image10",
-  "image11",
+  "./assets/image1.jpg",
+  "./assets/image2.jpg",
+  "./assets/image3.jpg",
+  "./assets/image4.jpg",
+  "./assets/image5.jpg",
+  "./assets/image6.jpg",
+  "./assets/image7.jpg",
+  "./assets/image8.jpg",
+  "./assets/image9.jpg",
+  "./assets/image10.jpg",
+  "./assets/image11.jpg",
 ];
 
 let imagesDOM = document.querySelector(".img");
@@ -30,13 +18,23 @@ let imagesDOM = document.querySelector(".img");
 function decrement() {
   if (pointer == 0) pointer = images.length;
   pointer--;
-  imagesDOM.src = `./assets/image${pointer + 1}.jpg`;
-  //   console.log(pointer);
+  imagesDOM.src = images[pointer];
 }
 
 function increment() {
   if (pointer == images.length - 1) pointer = -1;
   pointer++;
-  imagesDOM.src = `./assets/image${pointer + 1}.jpg`;
+  imagesDOM.src = images[pointer];
   //   console.log(pointer);
 }
+
+const preloadImage = (src) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = src;
+    resolve();
+  });
+
+(async function (params) {
+  await Promise.all(images.map((x) => preloadImage(x)));
+})();
