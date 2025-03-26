9e64b5f17dbbbd052cf724e742ab31e39580c394
Vicky <vickykotak09@gmail.com>
2025-03-26 10:08:35 +0530

    test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import image1 from "./assets/image1.jpg";
+import image2 from "./assets/image2.jpg";
+import image3 from "./assets/image3.jpg";
+import image4 from "./assets/image4.jpg";
+import image5 from "./assets/image5.jpg";
+import image6 from "./assets/image6.jpg";
+import image7 from "./assets/image7.jpg";
+import image8 from "./assets/image8.jpg";
+import image9 from "./assets/image9.jpg";
+import image10 from "./assets/image10.jpg";
+import image11 from "./assets/image11.jpg";
+
 let pointer = 0;
 let images = [
   "image1",
